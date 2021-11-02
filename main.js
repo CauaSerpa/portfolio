fb79ddbf763a20ee3e232ab52a6715cc9d78c20d
CauaSerpa <91634425+CauaSerpa@users.noreply.github.com>
2021-11-01 22:35:46 -0300

    Update main.js

--- main.js
+++ main.js
@@ -43,6 +43,7 @@ const sr = ScrollReveal({
 sr.reveal('.home__title',{})
 sr.reveal('.home__text',{delay: 200})
 sr.reveal('.img__disquete',{delay: 400})
+sr.reveal('.home__seta',{delay: 600})
 //scroll project
 sr.reveal('.project__img',{interval: 200})
 //scroll contact
